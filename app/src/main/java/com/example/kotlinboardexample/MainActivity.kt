668a16462f65b400258145e2f3d43a16d1b6687d
HudyKim <cmr3118@naver.com>
2021-08-24 17:40:33 +0900

    Second

--- app/src/main/java/com/example/kotlinboardexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinboardexample/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlinboardexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinboardexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+    var text: String = "text"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainBinding.mainActivity = this@MainActivity
+
+        text = "Change"
     }
 }
